b4cec45f98b178090a0b2ddb5a1eb50a5d59b138
e20sergeevich <93180648+e20sergeevich@users.noreply.github.com>
2022-01-11 15:51:18 +0300

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,20 @@
+// Why-we
+
+new Swiper(".review-slider__slider", {
+	navigation: {
+		nextEl: ".review-slider__slider-btn-next",
+		prevEl: ".review-slider__slider-btn-prev",
+		clickable: true
+	},
+	slidesPerView: 1,
+	slidesPerGroup: 1,
+	spaceBetween: 30
+});
+
+
+
+
+
+
+
+
